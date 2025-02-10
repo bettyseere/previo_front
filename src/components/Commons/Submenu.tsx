3c6bf0c3de2f0856c773ff702aab64183ea04361
Joshuamjv2 <joshuamjv22@gmail.com>
2025-02-10 21:40:32 +0300

    fix https error

--- src/components/Commons/Submenu.tsx
+++ src/components/Commons/Submenu.tsx
@@ -3,6 +3,7 @@ import Layout from "../../Layout/Dashboard/Layout";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useAuth } from "../../utils/hooks/Auth";
+import { Link } from "react-router-dom";
 
 interface props {
     children: React.ReactNode
@@ -29,9 +30,9 @@ export default function SubMenu({children, sub_page_props}: props) {
                 {is_super && <div className="flex justify-between gap-1">
                     {navigation.map(item=>{
                         const isActive = location.pathname === item.link;
-                        return (<div key={item.name} onClick={()=>navigate(item.link)} className={`w-full text-center font-semibold ${isActive ? "bg-tertiary": "bg-primary"} p-4 hover:scale-95 text-white cursor-pointer hover:duration-150`}>
+                        return (<Link to={item.link}><div key={item.name} className={`w-full text-center font-semibold ${isActive ? "bg-tertiary": "bg-primary"} p-4 hover:scale-95 text-white cursor-pointer hover:duration-150`}>
                         {item.name}
-                    </div>)})}
+                    </div></Link>)})}
                 </div>}
                 <Outlet /><div className="pt-4 px-4">{children}</div>
             </Layout>
