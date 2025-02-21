f59337a036d1d67d4a1273a1723b7c772b2bba8f
Joshuamjv2 <joshuamjv22@gmail.com>
2025-02-21 16:01:39 +0300

    switch secondary colors

--- src/components/Users/Form.tsx
+++ src/components/Users/Form.tsx
@@ -11,8 +11,8 @@ export default function UserForm() {
     return (
         <div>
             <div className="flex justify-between text-white font-bold">
-                <h2 className={`cursor-pointer p-4 ${!company_user ? "bg-tertiary": "bg-primary"} w-1/2`} onClick={()=>setCompanyUser(false)}>Super User</h2>
-                <h2 className={`cursor-pointer p-4 ${company_user ? "bg-tertiary": "bg-primary"} w-1/2`} onClick={()=>setCompanyUser(true)}>Company User</h2>
+                <h2 className={`cursor-pointer p-4 ${!company_user ? "bg-secondary": "bg-primary"} w-1/2`} onClick={()=>setCompanyUser(false)}>Super User</h2>
+                <h2 className={`cursor-pointer p-4 ${company_user ? "bg-secondary": "bg-primary"} w-1/2`} onClick={()=>setCompanyUser(true)}>Company User</h2>
             </div>
             {company_user ? <CompanyUserForm /> : <SuperUserForm />}
         </div>
