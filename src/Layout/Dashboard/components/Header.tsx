5bcd39a41992338da2121d3b88cefd70e4a7b4ec
Joshuamjv2 <joshuamjv22@gmail.com>
2025-08-18 11:37:46 +0300

    fix staff logo

--- src/Layout/Dashboard/components/Header.tsx
+++ src/Layout/Dashboard/components/Header.tsx
@@ -5,9 +5,9 @@ export default function DashboardHeader(){
     const { currentUser, handleLogout } = useAuth()
     const is_staff = currentUser?.user_type === "staff"
     return (
-        <div className="h-[2rem] rounded-md flex items-center my-2">
+        <div className="h-[2.5rem] rounded-md flex items-center my-2">
             <div className="flex justify-between items-center w-full">
-                <div className=" px-2 gap-2 mx-[.9rem]">{is_staff && <SeereLogo stying="" />}</div>
+                <div className=" px-2 gap-2 mx-[.9rem]">{is_staff && <SeereLogo styling="mt-0 py-2" size={50}/>}</div>
                 <div className="flex md:gap-8 lg:gap-12 text-gray-800 mr-5">
                     {is_staff ?<div className="cursor-pointer font-bold text-red-500" onClick={handleLogout}>Logout</div> :
                     <a href="/profile">
--- src/Layout/Dashboard/components/SeereLogo.tsx
+++ src/Layout/Dashboard/components/SeereLogo.tsx
@@ -1,9 +1,9 @@
-export default function SeereLogo({styling}: {stying?: string}){
+export default function SeereLogo({styling, size}: {styling?: string, size?: number}){
     return (
         <div className="">
             <img
                 className={`${styling ? styling: "p-0 mt-4"}`}
-                width={80}
+                width={size || 80}
                 src="/images/logo_seere/png/previo.png"
                 alt="seere logo"
             />
