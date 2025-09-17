e09d5acda0cb6629f803502bbdc7cd745476c4e6
Joshuamjv2 <joshuamjv22@gmail.com>
2025-09-17 12:45:33 +0300

    fix jh

--- src/components/Users/UserInfo/Teams/Reports/TeamRecords/Table.tsx
+++ src/components/Users/UserInfo/Teams/Reports/TeamRecords/Table.tsx
@@ -178,9 +178,9 @@ export default function UserTeamRecords() {
         if (row.original.ft || row.original.measurement_id === "d4ebb79e-a0a8-4550-8bc4-e4336b8490a3") {
         let val;
         if (row.original.measurement_id === "d4ebb79e-a0a8-4550-8bc4-e4336b8490a3"){
-          val = 4.903 * Math.pow((row.original.results/1000)/2, 2);
+          val = 4.903 * Math.pow((row.original.results/1000)/2, 2)*1000 ;
         } else {
-          val = 4.903 * Math.pow((row.original.ft/1000)/2, 2);
+          val = 4.903 * Math.pow((row.original.ft/1000)/2, 2)*1000;
         }
         return <div className="text-xs">{(val/1000).toFixed(3)} m</div>;}
       },
